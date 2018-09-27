5957ca7c266a0b19fc4a69a32e47063380715646
Nicolas Vahlas <nico@MacBook-Pro-de-administrateur.local>
2018-09-27 19:42:15 +0200

    validation done

--- CreateRating/index.js
+++ CreateRating/index.js
@@ -1,34 +1,39 @@
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    /*
     if (req.body) {
-        await performRequest('/api/GetUser?userId=' + req.body.userId, context);
-    } else {
-        context.res = {
-            status: 400,
-            body: "Please pass a name on the query string or in the request body"
-        };
-    }
-    */
-
-    if (req.body) {
-        await Promise.all( [getUser(req.body.userId), getProduct(req.body.productId)] ).then(
+        await Promise.all( [getUser(req.body.userId), getProduct(req.body.productId), checkRating(req.body.rating)] ).then(
             function(exists) {
+                context.res.body = 'All validation passed !';
+                context.done();
                 return exists;
             },
             function(e) {
+                context.res.body = 'Validation failed !';
+                context.done();
                 return false;;
             } 
         );
     }
+
+
+};
+
+function checkRating(rating) {
+    return new Promise(function(resolve, reject) {
+        if (rating >= 0 && rating <= 5) {
+            resolve(true);
+        } else {
+            reject();
+        }
+    });
 };
 
 function getProduct(productId) {
     const https = require('https');
 
     const options = {
-        hostname: 'serverlessohuser.trafficmanager.net',
+        hostname: 'serverlessohproduct.trafficmanager.net',
         port: 443,
         path: "/api/GetProduct?productId="+productId,
         method: 'GET'
@@ -36,13 +41,15 @@ function getProduct(productId) {
 
     return new Promise(function(resolve, reject) {
         const req = https.request(options, (res) => {
-            res.on('data', (d) => {
-                resolve(d?true:false);
-            });
+            if (res.statusCode != 200) {
+                reject();
+            } else {
+                resolve(true);
+            }
         });
 
         req.on('error', (e) => {
-            reject(e);
+            reject();
         });
 
         req.end();
@@ -61,13 +68,15 @@ function getUser(userId) {
 
     return new Promise(function(resolve, reject) {
         const req = https.request(options, (res) => {
-            res.on('data', (d) => {
-                resolve(d?true:false);
-            });
+            if (res.statusCode != 200 ) {
+                reject();
+            } else {
+                resolve(true);
+            }
         });
 
         req.on('error', (e) => {
-            reject(e);
+            reject();
         });
 
         req.end();
