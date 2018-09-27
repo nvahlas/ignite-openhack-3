53661f9dd0fbba1b4d2f01753d4a7fe505207dba
CarlosPoventud <carlospoventud@newhorizonsoftware.com>
2018-09-27 09:52:30 -0400

    -Get working

--- GetProduct/index.js
+++ GetProduct/index.js
@@ -1,10 +1,11 @@
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    if (req.query.name || (req.body && req.body.name)) {
+    if (req.query.productId) {
+        var productId = req.query.productId;
         context.res = {
             // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
+            body: "The product name for your product id " + productId + " is Starfruit Explosion",
         };
     }
     else {
